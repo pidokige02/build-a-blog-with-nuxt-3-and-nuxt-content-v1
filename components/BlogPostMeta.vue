ae269473424d8e0b443a4678f51629d0979d61a9
pidokige02 <pidokige0204@gmail.com>
2022-11-11 23:35:05 +0900

    final submit

---
+++ components/BlogPostMeta.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  author: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    default: 'info'
+  }
+})
+
+const formattedDate = computed(() => {
+  return new Date(props.date).toDateString()
+})
+</script>
+
+<template>
+  <div class="tags has-addons level-item are-medium">
+    <!-- <pre>
+      {{ (color)? color : "no color"}}
+    </pre> -->
+    <span class="tag is-rounded" :class="`is-${color}`">{{ author }}</span>
+    <span class="tag is-rounded">{{ formattedDate }}</span>
+  </div>
+</template>
+
+<style></style>
